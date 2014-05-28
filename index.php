43af2f849a1bca73eb7677e8455909da4ed151fc
Daniel Troger <daniel@natur-kultur.eu>
2014-05-28 18:43:28 +0200

    first beginning

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 header("Content-type: text/plain");
 print_r($_SERVER); //best beginning evar
 $date = date("m-d-Y H:i:s ");// best date evar
-echo "\n\n{$date}";
+echo "{$date} {$_SERVER['REMOTE_ADDR']}:{$_SERVER['SERVER_PORT']} {$_SERVER['SERVER_PROTOCOL']} {$_SERVER['REQUEST_METHOD']} {$_SERVER['REQUEST_URI']}";
 ?>
